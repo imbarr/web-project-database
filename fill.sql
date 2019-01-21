6d9a27e5ea63f65960e56a6c355613efe163aee1
imbar <imbrkvskh@gmail.com>
2019-01-21 22:48:38 +0500

    added file for testing

---
+++ fill.sql
@@ -0,0 +1,37 @@
+-- use spa;
+-- delete from payments where 1 = 1;
+-- delete from requests where 1 = 1;
+-- go
+
+use spa;
+insert into payments values ('1111111111111111', 1, 19, '000', 1000, '', 'awadad@gmail.com', 1)
+insert into payments values ('3453535434534555', 4, 22, '111', 10000, 'Good', 'awadad@yandex.com', 1)
+insert into payments values ('9999432499999349', 9, 19, '123', 33333, 'Bad', 'awadad@mail.ru', 1)
+insert into payments values ('9999478787799349', 8, 20, '122', 33443, 'Awesome', 'gachibass@gmail.com', 1)
+insert into payments values ('9879789898999349', 7, 21, '123', 43234, 'Bad', 'fghfhgffhfh@mail.ru', 1)
+insert into payments values ('9999432499999349', 6, 25, '333', 65665, 'Bad', 'ghffghfghfghfh@mail.ru', 1)
+insert into payments values ('7897899789999949', 5, 29, '444', 75000, 'Bad', 'fghfgh@mail.ru', 1)
+insert into payments values ('9999432499999349', 4, 30, '456', 34545, 'Bad', 'bbfbfbg@mail.ru', 1)
+insert into payments values ('9999432499999349', 3, 25, '345', 56655, 'Bad', 'bffgbfbg@mail.ru', 1)
+insert into payments values ('9789789898988949', 11, 25, '567', 2343, 'Bad', 'bgff@mail.ru', 1)
+insert into payments values ('9999432499999349', 12, 24, '056', 5656, 'Bad', 'bgffbgfg@mail.ru', 1)
+insert into payments values ('9978978978987849', 1, 23, '534', 2332, 'Bad', 'bgffbgf@mail.ru', 1)
+insert into payments values ('9789789789789849', 2, 35, '453', 1001, 'Bad', 'aaaaaaaaaaaaa@mail.ru', 1)
+
+insert into requests values ('123456789056', '123456489', '12345678901234567890', '10%', 1000, '+78005553535', 'ghffghfghfghfh@mail.ru')
+insert into requests values ('123456789053', '123456789', '12345678901234567890', '18%', 5656, '+79225553535', 'gh@mail.ru')
+insert into requests values ('123456789034', '154654589', '12345678901234567890', '18%', 5465, '+72835553535', 'gh54654564h@mail.ru')
+insert into requests values ('123456789034', '167575689', '12345678901234567890', '18%', 5465, '+78456465535', 'ghg4g45g4gghfh@mail.ru')
+insert into requests values ('123456789043', '125675689', '12345678901234567890', '10%', 5456, '+78456454535', 'g454g45ghfh@mail.ru')
+insert into requests values ('123456789034', '123456789', '12345678901234567890', 'None', 5456, '+78054643535', 'g454g45gghfghfh@mail.ru')
+insert into requests values ('123456789034', '123456789', '12345678901234567890', 'None', 75000, '+78005553535', '5g4g54g54hfghfh@mail.ru')
+insert into requests values ('123456789034', '125677689', '12345634534454567890', 'None', 10000, '+78546565535', 'j6j6j667fghfh@mail.ru')
+insert into requests values ('123456789032', '123456789', '12345678901234567890', 'None', 54656, '+78005553535', 'ghffghfghfghfh@mail.ru')
+insert into requests values ('123456789023', '123456789', '12345678901234567890', 'None', 65566, '+78005553535', '67j6j6j67jghfh@mail.ru')
+insert into requests values ('123456789000', '123456789', '12390990909234567890', 'None', 23234, '+78005553535', 'ghffghfghfghfh@mail.ru')
+insert into requests values ('1234567890', '123456789', '12345678345344444444', '18%', 42333, '+78005553535', 'ghffghfghfghfh@mail.ru')
+insert into requests values ('1234567890', '125667789', '12345678901234567890', '10%', 34334, '+78005553535', 'ghffghfghfghfh@mail.ru')
+insert into requests values ('1234567890', '123456789', '12345678901234567890', '10%', 34344, '+78005553535', 'ghffghfghfghfh@mail.ru')
+insert into requests values ('1234567890', '123456789', '12345678901234567890', '18%', 34344, '+78005553535', 'ghffghfghfghfh@mail.ru')
+insert into requests values ('1234567890', '659566689', '12300009090234567890', '18%', 1656, '+78005553535', 'ghffghfghfghfh@mail.ru')
+insert into requests values ('1234567890', '123456789', '12345678901234567890', '18%', 56566, '+78005553535', 'ghffghfghfghfh@mail.ru')
